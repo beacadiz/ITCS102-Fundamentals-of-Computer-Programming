4ba6d83823d741fb434d5d3a6b5308688c952c37
beacadiz <beatricecadiz5@gmail.com>
2025-08-18 21:12:12 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,11 @@
+a = 5
+b = 5
+ 
+c = 6
+d = 5
+username = 'Beatrice'
+password = 'ikawlangsapatna123'
+print((username == 'Beatrice') and (password == 'ikawlangsapatna123'))
+#print( (a > b ) and (c < d) )
+#print( (a > b ) or (c < d) )
+#print(not((a > b ) or (c < d)))
