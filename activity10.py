78c9404a4c3a7ce67084a1465698bbe4357b57fd
beacadiz <beatricecadiz5@gmail.com>
2025-08-27 15:18:26 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "Beatrice"
+password = "mahalkita123"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if uname == username and pword == password:
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED")
