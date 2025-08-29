f3378d77e74581fbdc594f8e6d2a24aa51a2a6af
beacadiz <beatricecadiz5@gmail.com>
2025-08-29 23:09:59 +0800

    Add files via upload

---
+++ codechallenge3.py
@@ -0,0 +1,13 @@
+name = input ("What is your name ? ---› ")
+fare = eval(input("Fare fee ---> "))
+isStudent = input ("Are you currently a student (yes / no) ")
+
+if isStudent == 'yes' :
+	discount = fare * 0.2
+	#fare -= discount
+	new_fare = fare - discount
+	print("Hi ", name )
+	print("Your Discount is ", discount)
+	print("Your new fare is ", new_fare)
+else:
+	print ("Sorry", name, "You are not eligible for a student discount")
