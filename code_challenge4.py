962f96e92be756f1e567f2f2f2c5f5b33a61d977
beacadiz <beatricecadiz5@gmail.com>
2025-09-10 21:49:31 +0800

    Add files via upload

---
+++ code_challenge4.py
@@ -0,0 +1,31 @@
+print ("Welcome to the manga Reader Recommender!") 
+print ("Let me ask some question so I can recommend the exact Manga you need!")  
+genre = input("What genre do you like to read? (action, romance, horror): ")
+duration = input("How long should the Manga be? (short, medium, long): ") 
+if genre == "horror" and duration == "long":
+	print("I recommend you Bleach!")
+
+if genre == "horror" and duration == "medium":
+	print("I recommend you Monster !")
+
+if genre == "horror" and duration == "short":
+	print("I recommend you Jujutsu Kaisen!")
+
+if genre == "horror" and duration == "long":
+	print("I recommend you The Kurosagi Corpse Delivery Service!")
+
+if genre == "horror" and duration == "medium":
+	print("I recommend you Mieruko-chan!")
+
+if genre == "horror" and duration == "short":
+	print("I recommend you Uzumaki!")
+
+if genre == "horror" and duration == "long":
+	print("I recommend you I Am a Hero!")
+
+if genre == "horror" and duration == "medium":
+	print("I recommend you Gyo!")
+
+if genre == "horror" and duration == "short":
+	print("I recommend you Parasyte!")
+
