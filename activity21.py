3d6ce93007fc0feddcc17af472514593cdc52343
beacadiz <beatricecadiz5@gmail.com>
2025-10-15 14:53:29 +0800

    Add files via upload

---
+++ activity21.py
@@ -0,0 +1,21 @@
+
+#while loop
+#laundry analogy
+#keywords -- while, continue, break
+#requirements - boolean variable
+
+isDirty = True #boolean initial value
+sum = 0
+
+while isDirty == True:
+    confirm = input("Do you wish to continue washing (yes / no)").lower()
+    sum += 1
+
+    if confirm == 'yes':
+        print("Washing  Continues .... ")
+        continue
+    else:
+        print("Washing stops... ")
+        break
+
+    print("Number of cycle is", sum)
